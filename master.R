0b3e1ae2e1601a5603a044d37d1c4835b8d86ef3
yushixiu <143405810+yushixiu@users.noreply.github.com>
2024-03-27 13:13:32 +0800

    Update master.R

--- master.R
+++ master.R
@@ -4,11 +4,12 @@ WD.PATH = paste(getwd(),'/Data-and-Code-CAM/',sep="")
 # Re-produce Figure 1
 source(paste(WD.PATH, 'code/fig1.R', sep=''))
 
-# Re-produce Experiment 1 results: Figure 2 & Table 1
+# Re-produce Experiment 1 results: Figure 2 & Table 1, Table 2
 m=100
 source(paste(WD.PATH, 'code/simulation1.R', sep=''))
 source(paste(WD.PATH, 'code/fig2.R', sep=''))
 source(paste(WD.PATH, 'code/table1.R', sep=''))
+source(paste(WD.PATH, 'code/table2.R', sep=''))
 
 # Re-produce Experiment 2 results: Table S.1, Table S.2, Table S.3, Table S.4
 m=100
@@ -17,18 +18,18 @@ source(paste(WD.PATH, 'code/tableS.2.R', sep=''))
 source(paste(WD.PATH, 'code/tableS.3.R', sep=''))
 source(paste(WD.PATH, 'code/tableS.4.R', sep=''))
 
-# Re-produce real data (Automobile dataset) results: Table 2, Figure 3, Figure 4, Figure 5, Figure 6
-source(paste(WD.PATH, 'code/table2.R', sep=''))
+# Re-produce real data (Automobile dataset) results: Table 3, Figure 3, Figure 4, Figure 5, Figure 6
+source(paste(WD.PATH, 'code/table3.R', sep=''))
 source(paste(WD.PATH, 'code/fig3.R', sep=''))
 source(paste(WD.PATH, 'code/fig4.R', sep=''))
 source(paste(WD.PATH, 'code/fig5.R', sep=''))
 source(paste(WD.PATH, 'code/fig6.R', sep=''))
 
-# Re-produce real data (CKD dataset) results: Table 4, Table 5, Table 6, Table 7, Figure 7, Figure 8
-source(paste(WD.PATH, 'code/table4.R', sep=''))
+# Re-produce real data (CKD dataset) results: Table 5, Table 6, Table 7, Table 8, Figure 7, Figure 8
 source(paste(WD.PATH, 'code/table5.R', sep=''))
 source(paste(WD.PATH, 'code/table6.R', sep=''))
 source(paste(WD.PATH, 'code/table7.R', sep=''))
+source(paste(WD.PATH, 'code/table8.R', sep=''))
 source(paste(WD.PATH, 'code/fig7.R', sep=''))
 source(paste(WD.PATH, 'code/fig8.R', sep=''))
 
